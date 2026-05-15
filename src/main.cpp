79660b96ec7c62d92890d1e6f4b4d758bfb78a4a
Michal <michalszandarowski@gmail.com>
2026-05-15 13:30:14 +0200

    fix: comment code that uses classes that do not exist yet

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,6 @@
 #include <Runtime.hpp>
-#include <Parser.hpp>
-#include <iostream>
 
 int main(int argc, char* argv[]) {
     Runtime::start();
-    try {
-        Parser parser;
-        std::cout << "Rozpoczynam parsowanie pliku...\n";
-        
-        
-        // Podaj ścieżkę do wygenerowanego z .proto pliku binarnego .pb
-        auto scene = parser.parse("tests/test_scene.pb"); 
-        
-        std::cout << "\n--- SUKCES ---\n";
-        std::cout << "Zaladowano projekt: " << scene->getExperimentName() << "\n";
-        std::cout << "Liczba obiektow w scenie: " << scene->getObjects().size() << "\n";
-        
-    } catch (const std::exception& e) {
-        std::cerr << "Blad parsowania: " << e.what() << '\n';
-    }
     return 0;
 }
