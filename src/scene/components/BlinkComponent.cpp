9f94b3c7fd34a11657573d5452e1d1e48e79b32d
Michal <michalszandarowski@gmail.com>
2026-05-17 23:29:41 +0200

    fix: format code

--- src/scene/components/BlinkComponent.cpp
+++ src/scene/components/BlinkComponent.cpp
@@ -9,7 +9,8 @@ std::unique_ptr<Component> BlinkComponent::createBlinker(const NeuronIDE::Compon
     return std::make_unique<BlinkComponent>(protoComp.blinker().blink_frequency_hz());
 }
 
-// Anonymous namespace to register the BlinkComponent creator in the ComponentRegistry at program startup
+// Anonymous namespace to register the BlinkComponent creator in the ComponentRegistry at program
+// startup
 namespace {
 struct BlinkRegistrar {
     BlinkRegistrar() {
