8ad1ff8d795c3a5e4f29bb11e20ee33eb06ed019
Michal <michalszandarowski@gmail.com>
2026-05-17 05:29:50 +0200

    fix: format code

--- src/parser/Parser.cpp
+++ src/parser/Parser.cpp
@@ -1,14 +1,12 @@
 #include "parser/Parser.hpp"
 
-#include "scene/SceneAll.hpp"
-
 #include <google/protobuf/text_format.h>  // dla .pbtxt jeśli będzie potrzeba
 
 #include <fstream>
 #include <stdexcept>
 
-
-#include "neuronide.pb.h"  // wygenerowany przez protoc
+#include "neuronide.pb.h"
+#include "scene/SceneAll.hpp"
 
 std::shared_ptr<::Scene> Parser::parse(const std::string& filePath) {
     NeuronIDE::Scene protoScene;
@@ -22,7 +20,6 @@ std::shared_ptr<::Scene> Parser::parse(const std::string& filePath) {
         throw std::runtime_error("Parser: failed to parse protobuf from: " + filePath);
     }
 
-    // Budowanie natywnej sceny silnika
     auto scene = std::make_shared<::Scene>();
     scene->setExperimentName(protoScene.project_name());
 
@@ -37,7 +34,6 @@ std::shared_ptr<::Scene> Parser::parse(const std::string& filePath) {
 std::shared_ptr<SceneObject> Parser::buildSceneObject(const NeuronIDE::SceneObject& protoObj) {
     auto obj = std::make_shared<SceneObject>(protoObj.name(), protoObj.is_visible());
 
-    // Transform (jeśli wyciągnięty poza oneof)
     if (protoObj.has_transform()) {
         const auto& tra = protoObj.transform();
         obj->setTransform(tra.x(), tra.y(), tra.width(), tra.height(), tra.rotation());
@@ -57,25 +53,26 @@ std::unique_ptr<Component> Parser::buildComponent(const NeuronIDE::Component& pr
     using CT = NeuronIDE::Component::ComponentTypeCase;
 
     switch (protoComp.component_type_case()) {
-        // case CT::kRenderer: {
-        //     const auto& ren = protoComp.renderer();
-        //     return std::make_unique<SpriteRenderer>(ren.texture_path(), ren.img_path());
-        // }
+            // case CT::kRenderer: {
+            //     const auto& ren = protoComp.renderer();
+            //     return std::make_unique<SpriteRenderer>(ren.texture_path(), ren.img_path());
+            // }
 
-        // case CT::kText: {
-        //     const auto& txt = protoComp.text();
-        //     return std::make_unique<TextRenderer>(txt.text(), txt.font_path(), txt.font_size());
-        // }
+            // case CT::kText: {
+            //     const auto& txt = protoComp.text();
+            //     return std::make_unique<TextRenderer>(txt.text(), txt.font_path(),
+            //     txt.font_size());
+            // }
 
         case CT::kBlinker: {
             const auto& bli = protoComp.blinker();
             return std::make_unique<BlinkComponent>(bli.blink_frequency_hz());
         }
 
-        // case CT::kScript: {
-        //     const auto& scr = protoComp.script();
-        //     return std::make_unique<ScriptComponent>(scr.script_path());
-        // }
+            // case CT::kScript: {
+            //     const auto& scr = protoComp.script();
+            //     return std::make_unique<ScriptComponent>(scr.script_path());
+            // }
 
         default:
             return nullptr;
