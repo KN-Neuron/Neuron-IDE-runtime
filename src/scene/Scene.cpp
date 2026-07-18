24f664d968bf35ff7424629d988b03457f0bc4ee
M1KUS3Q <mikolaj.popik@icloud.com>
2026-07-18 15:25:51 +0200

    feat: add onSceneReady lifecycle and getEvent/resolveEvents to Component and Scene

--- src/scene/Scene.cpp
+++ src/scene/Scene.cpp
@@ -1,6 +1,10 @@
 #include "scene/Scene.hpp"
 
+#include <stdexcept>
+
+#include "events/Delegate.hpp"
 #include "scene/SceneObject.hpp"
+#include "scene/components/Component.hpp"
 
 void Scene::update(const Context& ctx) {
     for (const auto& obj : objects) {
@@ -14,4 +18,30 @@ void Scene::render(SDL_Renderer* renderer) {
             obj->render(renderer);
         }
     }
+}
+
+void Scene::onSceneReady() {
+    for (const auto& obj : objects) {
+        for (const auto& comp : obj->components) {
+            comp->onSceneReady(*this);
+        }
+    }
+}
+
+Delegate& Scene::resolveEvents(std::string_view objectName, std::string_view eventName) {
+    for (const auto& obj : objects) {
+        if (obj->name != objectName) {
+            continue;
+        }
+        for (const auto& comp : obj->components) {
+            auto* delegate = comp->getEvent(eventName);
+            if (delegate != nullptr) {
+                return *delegate;
+            }
+        }
+        throw std::runtime_error("Scene::resolveEvents: event '" + std::string(eventName) +
+                                 "' not found on object '" + std::string(objectName) + "'");
+    }
+    throw std::runtime_error("Scene::resolveEvents: object '" + std::string(objectName) +
+                             "' not found");
 }
